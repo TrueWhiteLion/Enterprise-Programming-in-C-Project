4a3f345bc4c920232fbbfa1a34a65246437d138f
TrueWhiteLion <53228515+TrueWhiteLion@users.noreply.github.com>
2024-08-25 12:50:34 +0200

    Fixed formatting on images on the Product info pages + tested with external links.

--- ProductService.cs
+++ ProductService.cs
@@ -14,7 +14,7 @@ namespace Enterprise_Programming_in_C_Project.Services
                 Name = "Yeet Yeet Yite Test",
                 Description = "This has some stuff in that I want to see how it looks.",
                 Price = 200.00m,
-                ImageUrl = "/images/product1.jpg"
+                ImageUrl = "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             },
             new Product
             {
@@ -22,7 +22,7 @@ namespace Enterprise_Programming_in_C_Project.Services
                 Name = "Some more stuff that needs to go",
                 Description = "Backend to frontend stuff because funny and yes",
                 Price = 450.00m,
-                ImageUrl = "/images/product2.jpg"
+                ImageUrl = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXh1ZmNvcWQ3dnVhdjMwb25zdXZqdmZ5ZGtwMDA1bWxoNXA3cG53MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2A75RyXVzzSI2bx4Gj/giphy.webp"
             }
         };
 
